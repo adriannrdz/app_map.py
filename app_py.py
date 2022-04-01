63c75c66f32abbeedee294905d403a411c83af76
adriannrdz <102832144+adriannrdz@users.noreply.github.com>
2022-04-01 14:58:15 -0700

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -3,3 +3,6 @@ import pandas as pd
 #El pandas sirve para manipular las tabulaciones que tenemos en el Csv
 
 df= pd.read_csv('https://raw.githubusercontent.com/adriannrdz/app_map.py/main/data.csv')
+
+st.write(df)
+st.map(df)
