8a20b8fff01edc4d2b5acefb0ec477f60bb660d5
adriannrdz <102832144+adriannrdz@users.noreply.github.com>
2022-04-01 14:55:51 -0700

    Create app_py.py

---
+++ app_py.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+#El pandas sirve para manipular las tabulaciones que tenemos en el Csv
+
+df= pd.read_csv('https://raw.githubusercontent.com/adriannrdz/app_map.py/main/data.csv')
