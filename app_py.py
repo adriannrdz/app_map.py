eb3c949b17c6ef593e9626093f8f5e1a70c03985
adriannrdz <102832144+adriannrdz@users.noreply.github.com>
2022-04-26 10:24:15 -0600

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -1,8 +1,11 @@
 import streamlit as st
-import pandas as pd
-#El pandas sirve para manipular las tabulaciones que tenemos en el Csv
 
-df= pd.read_csv('https://raw.githubusercontent.com/adriannrdz/app_map.py/main/data.csv')
-
-st.write(df)
-st.map(df)
+st.balloons()
+st.header("Ad Astra")
+.streamlit/config.toml:
+    [theme]
+  primaryColor = "#d33682"
+  backgroundColor = "#000000"
+  secondaryBackgroundColor = "#586e75"
+  textColor = "#fafafa"
+  font = "nasalization"
