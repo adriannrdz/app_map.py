9d522c5c434a6ec6da7b26fdbcdcfdedebb70d39
adriannrdz <102832144+adriannrdz@users.noreply.github.com>
2022-04-26 10:46:13 -0600

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -2,10 +2,3 @@ import streamlit as st
 
 st.balloons()
 st.header("Ad Astra")
-.streamlit/config.toml:
-    [theme]
-  primaryColor = "#d33682"
-  backgroundColor = "#000000"
-  secondaryBackgroundColor = "#586e75"
-  textColor = "#fafafa"
-  font = "nasalization"
